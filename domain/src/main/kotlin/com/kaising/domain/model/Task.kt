3b93c7da70432f97ce145ee5f79ae32fae9dc31d
pedrojues99 <pedrojues99@gmail.com>
2025-06-12 00:01:40 +0200

    State added

--- domain/src/main/kotlin/com/kaising/domain/model/Task.kt
+++ domain/src/main/kotlin/com/kaising/domain/model/Task.kt
@@ -1,3 +1,10 @@
 package com.kaising.domain.model
 
-data class Task(val id: Int, val title: String, val isCompleted: Boolean, val description: String? = null)
+data class Task(val id: Int, val title: String, val isCompleted: Boolean, val description: String? = null, val state : State = State.PENDING)
+
+enum class State {
+    COMPLETED,
+    PENDING,
+    ARCHIVED,
+    DELETED
+}
