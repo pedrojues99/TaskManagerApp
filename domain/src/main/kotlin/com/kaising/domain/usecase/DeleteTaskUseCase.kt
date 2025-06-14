73665dffda9d4a4f529f8cc5d59182d0b0378d38
pedrojues99 <pedrojues99@gmail.com>
2025-06-14 22:34:46 +0200

    Common UI added

--- domain/src/main/kotlin/com/kaising/domain/usecase/DeleteTaskUseCase.kt
+++ domain/src/main/kotlin/com/kaising/domain/usecase/DeleteTaskUseCase.kt
@@ -1,11 +1,13 @@
 package com.kaising.domain.usecase
 
+
 import com.kaising.domain.model.Task
+import com.kaising.domain.model.TaskState
 import com.kaising.domain.repository.TaskRepository
 import javax.inject.Inject
 
 class DeleteTaskUseCase @Inject constructor(private val repository: TaskRepository) {
     suspend operator fun invoke(task: Task) {
-        repository.deleteTask(task)
+        repository.deleteTask(task.copy(state = TaskState.DELETED))
     }
 }
